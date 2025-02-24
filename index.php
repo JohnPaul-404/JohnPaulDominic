a69e27acf3e517007561a73b475bf62bb27ea8cf
JonConstantine01 <johnpaulghost17@gmail.com>
2025-02-24 13:46:41 +0530

    1st Commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>John Paul Dominic</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-container">
+            John <span> Paul </span><span>Dominic</span>
+        </div>
+        <div class="links">
+            <div class="link"><a href="#">Home</a></div>
+            <div class="link"><a href="#">About</a></div>
+            <div class="link"><a href="#">Services</a></div>
+            <div class="link"><a href="#">Skills</a></div>
+            <div class="link"><a href="#">Contact</a></div>
+        </div>
+    </nav>
+</body>
+</html>
